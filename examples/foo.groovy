7070a21b7754f95fd89b8400a70badb6afdd341a
Benjamin R Doerr <tiggerizzy@gmail.com>
2011-09-14 00:01:41 -0400

    Going grails

--- examples/foo.groovy
+++
@@ -1,7 +0,0 @@
-def foo = {Object[] l->
-  l.inject 0 {s,i-> s += i }
-}
-
-String.metaClass.getSize = { delegate.size() }
-
-foo '123'.size, '12'.size
--- src/me/bendoerr/groovy/statemachine/Assertions.groovy
+++
@@ -1,8 +0,0 @@
-import me.bendoerr.groovy.statemachine
-
-class Assertions {
-  void assertValidKeys(Map m, List validKeys) {
-    List invalidKeys = m.keys - validKeys
-    if (!invalidKeys.isEmpty()) throw new IllegalArgumentException("Invalid key(s): ${invalidKeys.join(', ')}")
-  }
-}
